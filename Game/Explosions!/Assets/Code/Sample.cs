b1a3cff991e8c1f34b79d2992a207cd5b3ff6068
Eric Welwood <deusecww@hotmail.com>
2014-02-06 17:00:53 -0600

    Updated Testing Scene
    + Added Sample.cs
    + Added sample terrain
    + Implemented Ground Movement via 3rd Person Controller
    = Fixed 3rd Person Controller Animations
    *Most of these changes are from today's Unity Tutorial meeting.

---
+++ Game/Explosions!/Assets/Code/Sample.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sample : MonoBehaviour {
+
+    //This field is not visible in the inspector
+    private int privateField;
+
+    //This field is visible in the inspector
+    [SerializeField]
+    private int inspectorField;
+
+	// Use this for initialization (Only called once)
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    // Fixed(Physics) Update is called at a fixed interval
+    void FixedUpdate()
+    {
+
+    }
+}
