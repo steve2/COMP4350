a297f9a47c2ec1f61d2d42f15adbe3df1b0eff50
Eric Welwood <deusecww@hotmail.com>
2014-02-14 13:50:59 -0600

    Created Update Server menu
    + Added Items (Not implemented)
    = Updated Readme to include Editor folder

---
+++ Game/Explosions!/Assets/Code/Editor/UpdateItems.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class UpdateItems : MonoBehaviour 
+{
+    [MenuItem("Update Server/Items")]
+    private static void Run()
+    {
+        //TODO: Implement
+        Debug.Log("Not Implemented");
+    }
+}
+#endif
