ad7d02f080fdae53674b93fe14718bdb7a3dee03
Eric Welwood <deusecww@hotmail.com>
2014-02-16 13:08:52 -0600

    Updated TestWeapon to include namespace
    = Surrounded class in it's proper namespace

--- Game/Explosions!/Assets/Code/Editor/Tests/Components/TestWeapon.cs
+++ Game/Explosions!/Assets/Code/Editor/Tests/Components/TestWeapon.cs
@@ -7,27 +7,31 @@ using Assets.Code.Model;
 using System.Collections.Generic;
 using System.Linq;
 
-[ExecuteInEditMode]
-public class TestWeapon {
-    //TODO: Should consider making Weapon.Attributes and Weapon constructor internal and add InternalsVisibleTo
-
-    //Make sure the Weapon is being initialized properly (default)
-    [Test]
-    public void TestDefaultStart()
+namespace Assets.Code.Editor.Tests.Components
+{
+    [ExecuteInEditMode]
+    class TestWeapon
     {
-        //This is normally done automatically, but where peforming a Unit Test
-        Weapon.EditorAttributes eAttributes = new Weapon.EditorAttributes();
-        eAttributes.damage = 0;
-        eAttributes.extraAttributes = new List<ItemAttribute>();
+        //TODO: Should consider making Weapon.Attributes and Weapon constructor internal and add InternalsVisibleTo
 
-        Item item = new Weapon(eAttributes); //IGNORE WARNING
+        //Make sure the Weapon is being initialized properly (default)
+        [Test]
+        public void TestDefaultStart()
+        {
+            //This is normally done automatically, but where peforming a Unit Test
+            Weapon.EditorAttributes eAttributes = new Weapon.EditorAttributes();
+            eAttributes.damage = 0;
+            eAttributes.extraAttributes = new List<ItemAttribute>();
 
-        item.Start();
-        //Iterating through the list to find an item is inefficient,
-        //but this is a Unit Test and we don't want to give public read/write access to the attributes
-        IEnumerable<ItemAttribute> attributes = item.ItemAttributes; 
-        Assert.That(attributes.Single((x) => x.Type == AttributeType.ItemType).Value == (int)ItemType.Weapon);
-        Assert.That(attributes.Single((x) => x.Type == AttributeType.Damage).Value == 0);
+            Item item = new Weapon(eAttributes); //IGNORE WARNING
+
+            item.Start();
+            //Iterating through the list to find an item is inefficient,
+            //but this is a Unit Test and we don't want to give public read/write access to the attributes
+            IEnumerable<ItemAttribute> attributes = item.ItemAttributes;
+            Assert.That(attributes.Single((x) => x.Type == AttributeType.ItemType).Value == (int)ItemType.Weapon);
+            Assert.That(attributes.Single((x) => x.Type == AttributeType.Damage).Value == 0);
+        }
     }
 }
 #endif
