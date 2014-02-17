9b6a1f85d62133fd93f1d0f1dbd9e908a67e8804
Brahmdeep Singh Juneja <biker121@gmail.com>
2014-02-17 15:57:09 -0600

    Created initial Movement Buttons

---
+++ Game/Explosions!/Assets/Code/View/MovementButtons.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementButtons : MonoBehaviour {
+//	private static float currVerticalInputGUI = 0.0f;
+//
+//
+//	private static float verticalInputGUI = 0.0f;
+//	private static float verticalInputKey = 0.0f;
+//	private static float verticalInputFinal = 0.0f;
+//	private static float verticalInput = 0.0f;
+//
+//	private static float horizontalInputGUI = 0.0f;
+//	private static float horizontalInput = 0.0f;
+//	private static float horizontalFinal = 0.0f;
+//	private static float horizontalInputKey = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		verticalInputKey = Input.GetAxis( "Vertical" );
+//		verticalInputFinal = Mathf.Clamp (verticalInputKey + verticalInputGUI, -1.0f, 1.0f);
+//		horizontalInputKey = Input.GetAxis ("Horizontal");
+//		vertInputKey = Input.GetAxis( "Vertical" );		
+//		vertInputAll = Mathf.Clamp( vertInputKey + vertInputGUI, -1.0, 1.0 );  
+//		transform.position += transform.forward * vertInputAll * speed * Time.deltaTime;
+		
+	}
+
+	// Called when the GUI is rendered
+	void OnGUI() {
+//		var controller : CharacterController  = GetComponent (CharacterController );
+//		GUILayout.BeginArea (new Rect ((Screen.width * 0.5f) - 100, (Screen.height * 0.5f) - 100, 200, 300));
+//		GUI.Box (new Rect (Screen.width - 100,Screen.height - 50,100,50));
+
+		GUILayout.BeginArea (new Rect ((Screen.width * 0.5f) - 100, (Screen.height * 0.5f) - 100, 200, 300));
+		GUILayout.Button ("Single Player");
+		GUILayout.Button ("Multiplayer");
+		GUILayout.EndArea();
+	}
+}
+
+//		GUILayout.BeginArea (new Rect (0, (Screen.height) - 100, 100, 100));
+//		GUILayout.RepeatButton ("W");
+//
+//		if (GUILayout.RepeatButton ("W")) {
+//			Event.KeyboardEvent("" + KeyCode.W);
+//		}
+//
+//		GUILayout.EndArea();
+
+		//		if ( GUI.RepeatButton( new Rect( 10, 10, 100, 35 ), "W" ) )
+		//		{
+		//			Event.KeyboardEvent("" + KeyCode.W);
+		//		}
+		//		else if ( GUI.RepeatButton(new Rect( 10, 55, 100, 35 ), "S" ) )
+		//		{
+		//			verticalInputGUI = -1.0f;
+		//		}
+		//		else
+		//		{
+		//			verticalInputGUI = 0.0f;
+		//		}
+
+		//		if (GUILayout.Button ("A")) {
+		//			Event.KeyboardEvent("" + KeyCode.A);
+		//		}
+		
+		//		if (GUILayout.Button ("S")) {
+		//			Event.KeyboardEvent(KeyCode.S);
+		//		}
+		//		if (GUILayout.Button ("A")) {
+		//			Event.KeyboardEvent(KeyCode.A);
+		//		}
+		//		if (GUILayout.Button ("D")) {
+		//			Event.KeyboardEvent(KeyCode.D);
+		//		}
+//	}
+//}
