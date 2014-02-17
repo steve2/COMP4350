79fdc8cb377a075b2984b094facfb97c4846c153
Steve <b0rg3r@gmail.com>
2014-02-17 15:45:00 -0600

    More merging.

--- Server/backend/database.py
+++ Server/backend/database.py
@@ -23,20 +23,12 @@ TABL_NAME = "COMP4350_GRP5"
 #***************************************************************************
 #***************************************************************************
 
-def db_connect():
-    db = DB.connect(HOST_NAME, USER_NAME, USER_PASS, TABL_NAME)
-
 def print_players():
 	db = db_connect()
 	c = db.cursor()
 	c.execute("SELECT * FROM Player")
-<<<<<<< HEAD
-	print "\nPython-MySQL Results\n============================="
-	result = cursor.fetchone()
-=======
 	print "\nPython-DB Result Object\n============================="
 	result = c.fetchone()
->>>>>>> 8f1596ec03bd7442a6bfaef69374e1538d6d9b1d
 	while (result != None):
 		print "- ", result, "\n"
 		result = c.fetchone()
