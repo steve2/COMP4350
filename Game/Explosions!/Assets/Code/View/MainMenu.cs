ddad0beb37888b72a222fd7a3df45dcc82065696
Ethan Fedeniuk <EthanFedeniuk@gmail.com>
2014-02-17 14:11:13 -0600

    Link Main Menu to Demo

--- Game/Explosions!/Assets/Code/View/MainMenu.cs
+++ Game/Explosions!/Assets/Code/View/MainMenu.cs
@@ -18,7 +18,9 @@ public class MainMenu : MonoBehaviour {
 		GUILayout.BeginArea (new Rect ((Screen.width * 0.5f) - 100, (Screen.height * 0.5f) - 100, 200, 300));
 		GUILayout.Button ("Single Player");
 		GUILayout.Button ("Multiplayer");
-		GUILayout.Button ("Play Demo");
+		if (GUILayout.Button ("Play Demo")) {
+			Application.LoadLevel("Demo"); // Play the Demo 
+				}
 		GUILayout.Button ("View website");
 		GUILayout.Button ("Settings");
 		GUILayout.EndArea();
