edc9c8f2e3568b94511d7dae87398b79a2c122ee
Eric Welwood <deusecww@hotmail.com>
2014-02-17 15:01:31 -0600

    Created Character Component
    + Created Initial Interface
    + Created Initial Fields

---
+++ Game/Explosions!/Assets/Code/Components/Character.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
+{
+    #region Fields
+    private int health;
+    private int maxHealth;
+    [SerializeField]
+    private int exp;
+    [SerializeField]
+    private int level; 
+    #endregion
+
+    #region Properties
+    #region PercentHealth
+    public int PercentHealth
+    {
+        get
+        {
+            return (int)((float)health / (float)maxHealth) * 100;
+        }
+    } 
+    #endregion
+
+    public int Exp { get { return exp; } }
+
+    public int Level { get { return level; } } 
+    #endregion
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
