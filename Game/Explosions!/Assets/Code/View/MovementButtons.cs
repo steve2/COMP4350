e45193c1802e6c7558d698ddf354b1afd459b35e
Brahmdeep Juneja <brahmdeepj@gmail.com>
2014-02-20 05:30:57 -0600

    Movement implemented
    - camera rotation still requires work.

--- Game/Explosions!/Assets/Code/View/MovementButtons.cs
+++ Game/Explosions!/Assets/Code/View/MovementButtons.cs
@@ -2,18 +2,6 @@
 using System.Collections;
 
 public class MovementButtons : MonoBehaviour {
-//	private static float currVerticalInputGUI = 0.0f;
-//
-//
-//	private static float verticalInputGUI = 0.0f;
-//	private static float verticalInputKey = 0.0f;
-//	private static float verticalInputFinal = 0.0f;
-//	private static float verticalInput = 0.0f;
-//
-//	private static float horizontalInputGUI = 0.0f;
-//	private static float horizontalInput = 0.0f;
-//	private static float horizontalFinal = 0.0f;
-//	private static float horizontalInputKey = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,62 +10,39 @@ public class MovementButtons : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-//		verticalInputKey = Input.GetAxis( "Vertical" );
-//		verticalInputFinal = Mathf.Clamp (verticalInputKey + verticalInputGUI, -1.0f, 1.0f);
-//		horizontalInputKey = Input.GetAxis ("Horizontal");
-//		vertInputKey = Input.GetAxis( "Vertical" );		
-//		vertInputAll = Mathf.Clamp( vertInputKey + vertInputGUI, -1.0, 1.0 );  
-//		transform.position += transform.forward * vertInputAll * speed * Time.deltaTime;
-		
+
 	}
 
 	// Called when the GUI is rendered
 	void OnGUI() {
-//		var controller : CharacterController  = GetComponent (CharacterController );
-//		GUILayout.BeginArea (new Rect ((Screen.width * 0.5f) - 100, (Screen.height * 0.5f) - 100, 200, 300));
-//		GUI.Box (new Rect (Screen.width - 100,Screen.height - 50,100,50));
-
-        GUILayout.BeginArea(new Rect(0, (Screen.height) - 100, 100, 100));
-
-        if (GUILayout.RepeatButton("W"))
-        {
-            InputAdapter.SetAxisRaw("Vertical", 1);
-        }
-        else
-        {
-            InputAdapter.SetAxisRaw("Vertical", 0);
-        }
-
+		GUILayout.BeginArea (new Rect (0, (Screen.height) - 100, 100, 100));
+		if (GUILayout.RepeatButton ("W")) 
+		{
+			InputAdapter.SetAxisRaw ("Vertical", 1);
+			GUILayout.RepeatButton ("S");
+		} 
+		else if (GUILayout.RepeatButton ("S")) 
+		{
+			InputAdapter.SetAxisRaw ("Vertical", -1);
+		} 
+		else
+		{
+			InputAdapter.SetAxisRaw ("Vertical", 0);
+		}
+		
+		if (GUILayout.RepeatButton ("A"))
+		{
+			InputAdapter.SetAxisRaw ("Horizontal", -1);
+			GUILayout.RepeatButton("D");
+		}
+		else if ( GUILayout.RepeatButton("D"))
+		{
+			InputAdapter.SetAxisRaw("Horizontal", 1);
+		}
+		else
+		{
+			InputAdapter.SetAxisRaw("Horizontal", 0);
+		}
         GUILayout.EndArea();
     }
 }
-
-
-		//		if ( GUI.RepeatButton( new Rect( 10, 10, 100, 35 ), "W" ) )
-		//		{
-		//			Event.KeyboardEvent("" + KeyCode.W);
-		//		}
-		//		else if ( GUI.RepeatButton(new Rect( 10, 55, 100, 35 ), "S" ) )
-		//		{
-		//			verticalInputGUI = -1.0f;
-		//		}
-		//		else
-		//		{
-		//			verticalInputGUI = 0.0f;
-		//		}
-
-		//		if (GUILayout.Button ("A")) {
-		//			Event.KeyboardEvent("" + KeyCode.A);
-		//		}
-		
-		//		if (GUILayout.Button ("S")) {
-		//			Event.KeyboardEvent(KeyCode.S);
-		//		}
-		//		if (GUILayout.Button ("A")) {
-		//			Event.KeyboardEvent(KeyCode.A);
-		//		}
-		//		if (GUILayout.Button ("D")) {
-		//			Event.KeyboardEvent(KeyCode.D);
-		//		}
-//	}
-//}
