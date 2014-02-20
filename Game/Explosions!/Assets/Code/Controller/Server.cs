9d9d99176a24606da57ba08c248910b3777a54bb
Simon BD <simon@server>
2014-02-20 14:27:57 -0600

    Add Server connection to game client

---
+++ Game/Explosions!/Assets/Code/Controller/Server.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Script.Serialization.JavaScriptSerializer;
+
+class Server {
+    private String url;
+    private JavaScriptSerializer serializer;
+
+    public Server(String url) {
+        this.url = url;
+        this.serializer = new JavaScriptSerializer();
+    }
+
+    public async Task<Dictionary<String, Object>> Send(Object table) {
+        var json = serializer.serialize(table);
+         
+        var utf8 = new System.Text.UTF8Encoding();
+        var postHeader = new Hashtable();
+           
+        postHeader.Add("Content-Type", "text/json");
+        postHeader.Add("Content-Length", json.Length);
+         
+        var www = WWW(this.url, utf8.GetBytes(json), postHeader);
+         
+        yield www;
+           
+        return serializer.Deserialize(www.text);
+    }
+}
