f7e217b1027e0d96d47b62324c70a034ace9b894
Simon BD <simon@server>
2014-02-20 15:08:00 -0600

    Get Server to work

--- Game/Explosions!/Assets/Code/Controller/Server.cs
+++ Game/Explosions!/Assets/Code/Controller/Server.cs
@@ -1,28 +1,26 @@
 using System;
 using System.Collections.Generic;
-using System.Web;
-using System.Web.Script.Serialization.JavaScriptSerializer;
 using System.Threading.Tasks;
 
 using SimpleJSON;
 
 class Server {
     private String url;
-    private JavaScriptSerializer serializer;
 
     public Server(String url) {
         this.url = url;
         this.serializer = new JavaScriptSerializer();
     }
 
-    public async Task<JSON> Send(String json) {
+    private async Task<JSON> Send(String path, String json) {
         var utf8 = new System.Text.UTF8Encoding();
         var header = new Hashtable();
            
         header.Add("Content-Type", "text/json");
         header.Add("Content-Length", json.Length);
          
-        var www = WWW(this.url, utf8.GetBytes(json), header);
+        var location = this.url + "/" + path;
+        var www = WWW(location, utf8.GetBytes(json), header);
          
         yield return www;
            
