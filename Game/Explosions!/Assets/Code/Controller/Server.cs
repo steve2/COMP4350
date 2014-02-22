38d44d2fedc5e0085daaee2638a708b9c1460392
Eric Welwood <deusecww@hotmail.com>
2014-02-22 15:23:06 -0600

    Created Game "Service"
    + Added Game with a global Instance
    + Included dependency injection support
    = Refactored Server to move instance into Game

--- Game/Explosions!/Assets/Code/Controller/Server.cs
+++ Game/Explosions!/Assets/Code/Controller/Server.cs
@@ -8,33 +8,8 @@ using UnityEngine;
 
 public class Server 
 {
-    private static Server instance;
-    private const string PRODUCTION_URL = "http://54.200.201.50";
     private String url;
 
-    /// <summary>
-    /// This is a service that provides communication with a "server" (Whether it's a real server or not)
-    /// It can be changed during run-time
-    /// Options: ProductionServer, TestServer, DemoServer, ServerStub
-    /// </summary>
-    //public static Server Instance { get; set; }
-    //TODO: Maybe instead of instantiating a default server here, we could handle that in a setup elsewhere and use the cleaner property above
-    //TODO: This instance can be stored inside Server class, or inside a different global class
-    public static Server Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = new Server(PRODUCTION_URL);
-            }
-            return instance;
-        }
-        set
-        {
-            instance = value;
-        }
-    }
 
     public Server(String url) 
     {
