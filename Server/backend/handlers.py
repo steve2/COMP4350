303637efc4a44fc406b67ffad4e83aaef02b19ed
Steve <b0rg3r@gmail.com>
2014-02-25 23:30:54 -0600

    Copy and paste from Simon's commit as to not over-write his changes.

--- Server/backend/handlers.py
+++ Server/backend/handlers.py
@@ -22,7 +22,7 @@ def handle_new_account():
         name = data['user']
         password = data['password']
         password_hash = hash_password(password)
-	
+
         try:
             player.create_player(name, password_hash)
             session['username'] = name
@@ -54,10 +54,29 @@ def handle_login_request():
         else:
             result = {'result': False}
     return jsonify(result)
-    
+
+SHOP = -1 #Character ID for SHOP
 def use_recipe(recipe, inChar, outChar):
-    pass
-    #this function didn't compile for me.
+    inItems = recipe.get_recipe_in(recipe)
+    outItems = recipe.get_recipe_out(recipe)
+    success = false
+    #TODO: Verify the recipe is valid (Character has sufficient items in inventory)
+    if inChar != SHOP:
+         #TODO: Does this call make sense? Or should we loop and call contains_item
+        if not inventory.contains_items(inChar, inItems):
+            return false
+    if outChar != SHOP:
+        if not inventory.contains_items(outChar, outItems):
+            return false
+        
+    #TODO: Remove inItems from inChar (Make a common function)
+    #if(inChar != SHOP):
+        #loop through all items? (Or single function)
+        #inventory.remove(inChar, item)
+    #TODO: Remove outItems from outChar
+    #if(inChar != SHOP):
+        #loop through all items?
+        #inventory.remove(outChar, item)
     
 @app.route('/useRecipe', methods = ['POST', 'GET'])
 def handle_use_recipe():
@@ -114,4 +133,3 @@ def handle_is_alive():
     response = {"result" : "1"}
     return jsonify(response)
     
-
