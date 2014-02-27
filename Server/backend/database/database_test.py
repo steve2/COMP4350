5662fc549bf81e0be90725eb0c59828a9e1a6d18
Steve <b0rg3r@gmail.com>
2014-02-27 15:22:46 -0600

    Added more testing for Database code.

--- Server/backend/database/database_test.py
+++ Server/backend/database/database_test.py
@@ -22,7 +22,7 @@ class DatabaseTestCase (unittest.TestCase):
         self.curs.execute("CREATE TABLE IF NOT EXISTS Achievement (ID INT NOT NULL PRIMARY KEY, Name CHAR (64), Descr CHAR (255))")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Attribute (ID INT NOT NULL PRIMARY KEY, Text CHAR (64))")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Slot (ID INT NOT NULL PRIMARY KEY, Name CHAR(64))")
-        self.curs.execute("CREATE TABLE IF NOT EXISTS Player (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Username CHAR(255) NOT NULL, Password CHAR(255) NOT NULL)")
+        self.curs.execute("CREATE TABLE IF NOT EXISTS Player (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Username CHAR(255) NOT NULL UNIQUE, Password CHAR(255) NOT NULL)")
         self.curs.execute("CREATE TABLE IF NOT EXISTS `Character` (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Player_ID INTEGER NOT NULL, Name CHAR(64), Exp INT DEFAULT 0, Play_Time INT DEFAULT 0)")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Item_Type (ID INT NOT NULL PRIMARY KEY, Name CHAR(64))")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Item (ID INT NOT NULL PRIMARY KEY, Item_Type_ID INT NOT NULL, Name CHAR(64), Description CHAR(255), FOREIGN KEY (Item_Type_ID) REFERENCES Item_Type (ID))")
@@ -30,6 +30,10 @@ class DatabaseTestCase (unittest.TestCase):
         self.curs.execute("CREATE TABLE IF NOT EXISTS Inventory_Item (Character_ID INT NOT NULL, Item_ID INT NOT NULL, Quantity INT, PRIMARY KEY (Character_ID, Item_ID), FOREIGN KEY (Character_ID) REFERENCES `Character` (ID), FOREIGN KEY (Item_ID) REFERENCES Item (ID))")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Item_Attributes ( Item_ID INT NOT NULL, Attribute_ID INT NOT NULL, Value INT, FOREIGN KEY (Item_ID) REFERENCES Item (ID), FOREIGN KEY (Attribute_ID) REFERENCES Attribute (ID), PRIMARY KEY (Item_ID, Attribute_ID) )")
         self.curs.execute("CREATE TABLE IF NOT EXISTS Item_Slot (Item_Type_ID INT NOT NULL, Slot_ID INT NOT NULL, PRIMARY KEY (Item_Type_ID, Slot_ID), FOREIGN KEY (Item_Type_ID) REFERENCES Item_Type (ID), FOREIGN KEY (Slot_ID) REFERENCES Slot (ID))")
+        
+        self.curs.execute("INSERT INTO Player (Username, Password) VALUES ('Username', 'Password')")
+        self.curs.execute("INSERT INTO Player (Username, Password) VALUES ('Username2', 'Password')")
+        self.curs.execute("INSERT INTO `Character` (Player_ID, Name) VALUES (1, 'Character')")
         
     def tearDown (self):
         self.curs.execute("DROP TABLE IF EXISTS Achievement")
@@ -46,16 +50,33 @@ class DatabaseTestCase (unittest.TestCase):
         self.db.close()
         
     def test_create_player (self):
-        assert create_player(None, None) == False
-        assert create_player("TestUsername", None) == False
-        assert create_player(None, "TestPassword") == False
+        #Cannot create duplicate Usernames:
+        assert not create_player("Username", "Password")
+        assert not create_player("Username2", "Password")
+        #Invalid input:
+        assert not create_player(None, None)
+        assert not create_player("TestUsername", None) 
+        assert not create_player(None, "TestPassword") 
+        #Mixed input:
+        assert not create_player(None, "Password")
+        #Valid innput:
         assert create_player("TestUsername", "TestPassword")
         
     def test_get_player (self):
+        #Valid queries:
+        assert get_player("Username2", "Password") != None
+        assert get_player("Username", "Password") != None
+        #Invalid input:
         assert get_player("TestUsername", "TestPassword") == None
         assert get_player(None, None) == None
         assert get_player(None, "TestPassword") == None
         assert get_player("TestUsername", None) == None
+        #Mixed input:
+        assert get_player("Username2", "Bad Password") == None
+        assert get_player("Username", "Bad Password") == None
+        assert get_player("Bad Username", "Password") == None
+        assert get_player(None, "Password") == None
+        assert get_player("Username2", None) == None
         
     def test_get_player_id (self):
         assert get_player_id(None) == None
@@ -69,12 +90,17 @@ class DatabaseTestCase (unittest.TestCase):
         assert get_characters(None) == None
         assert get_characters("") == None
         assert get_characters("TestUsername") == None
+        assert get_characters("Username")
         
     def test_create_character  (self):
-        assert create_character(None, "TestCharName") == False
-        assert create_character(None, None) == False
-        assert create_character("TestUsername", None) == False
-        assert create_character("TestUsername", "TestCharname")
+        #invalid input
+        assert not create_character(None, "TestCharName") 
+        assert not create_character(None, None) 
+        assert not create_character("TestUsername", None)
+        #"TestUsername" does not exist, so a character cannot be created for it.
+        assert not create_character("TestUsername", "TestCharname")
+        #"Username" should work.
+        assert create_character("Username", "TestCharName")
         
     def test_get_items (self):
         assert get_items() != None
