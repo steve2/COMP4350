0508f75488a38e977ebb9567b28670714677b285
Simon BD <simon@server>
2014-02-28 00:02:28 -0600

    Add tests for equipment, and put suites in place for items and recipes, so we can test them when they are ready

--- Server/server_test.py
+++ Server/server_test.py
@@ -8,6 +8,7 @@ import backend.database.player as player
 import backend.database.character as character
 import backend.database.inventory as inventory
 import backend.database.item as item
+import backend.database.equipment as equipment
 
 # Test that all of our static webpages can be found and are getting loaded
 class TemplateTestCase(unittest.TestCase):
@@ -37,6 +38,10 @@ class TemplateTestCase(unittest.TestCase):
         rv = self.app.get('/character/new')
         assert rv.data != None
 
+#TODO: start using this
+def post(app, url, data):
+    return app.post(url, data=json.dumps(data), content_type='application/json')
+
 class HandlerTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
@@ -62,6 +67,21 @@ class HandlerTestCase(unittest.TestCase):
         data = json.loads(resp.data)
         assert data['result'] == False
 
+    def test_current_character_none(self):
+        resp = self.app.post('/player/current', data=json.dumps({}), content_type='application/json')
+        data = json.loads(resp.data)
+        assert data["result"] == None
+
+    def test_current_character_none(self):
+        username = 'UserJoe'
+
+        with self.app.session_transaction() as sess:
+            sess['username'] = username
+
+        resp = self.app.post('/player/current', data=json.dumps({}), content_type='application/json')
+        data = json.loads(resp.data)
+        assert data["result"] == username
+
 class CharacterHandlerTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
@@ -72,6 +92,8 @@ class CharacterHandlerTestCase(unittest.TestCase):
         item.reset_items()
         item.reset_item_types()
         item.reset_item_attributes()
+        equipment.reset_equipment()
+        equipment.reset_slots()
         player.create_player("UserJoe", "test")
 
     def tearDown(self):
@@ -132,6 +154,56 @@ class CharacterHandlerTestCase(unittest.TestCase):
 
         inventory = data["inventory"]
         assert len(inventory) == 0
+
+    def test_get_empty_equipped(self):
+        with self.app.session_transaction() as sess:
+            sess['username'] = 'UserJoe'
+
+        header = {"charname": "CharacterFred"}
+        resp = self.app.post('/character/create', data=json.dumps(header), content_type='application/json')
+        resp = self.app.post('/character/getAll', data=json.dumps({}), content_type='application/json')
+        data = json.loads(resp.data)
+
+        assert len(data) == 1 # Make sure we got a character
+
+        header = {"charid": data["characters"][0][0]}
+        resp = self.app.post('/character/equipped', data=json.dumps(header), content_type='application/json')
+
+        data = json.loads(resp.data)
+
+        inventory = data["equipment"]
+        assert len(inventory) == 0
+
+#TODO: Test items when DB tables are completed
+class ItemHandlerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        database.db_connect_test()
+        player.reset_players()
+        character.reset_characters()
+        inventory.reset_inventory()
+        item.reset_items()
+        item.reset_item_types()
+        item.reset_item_attributes()
+        player.create_player("UserJoe", "test")
+
+    def tearDown(self):
+        pass
+
+    def test_get_all_items(self):
+        pass
+
+    def test_get_item(self):
+        pass
+
+#TODO: Test recipes once the API has settled
+class RecipeHandlerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        database.db_connect_test()
+
+    def tearDown(self):
+        pass
 
 if __name__ == '__main__':
     app.secret_key = "Testing secret key"
