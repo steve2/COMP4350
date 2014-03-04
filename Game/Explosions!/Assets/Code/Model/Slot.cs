bac86699972bb36a33a65d3a93fb0c722443371f
Steve <b0rg3r@gmail.com>
2014-03-04 15:25:20 -0600

    Created Slot.cs for Slot Enum Type.

---
+++ Game/Explosions!/Assets/Code/Model/Slot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Code.Model
+{
+    //Identical to database
+    public enum ItemType
+    {
+        RightHand   = 1,
+        LeftHand    = 2,
+        Head        = 3,
+        Chest       = 4,
+        Legs        = 5
+    }
+}
