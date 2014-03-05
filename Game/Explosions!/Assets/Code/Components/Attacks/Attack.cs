8f8f9d16f5e8938507cedde702abfa1f5436fe2a
Eric Welwood <deusecww@hotmail.com>
2014-03-05 10:34:39 -0600

    Implemented RayAttack
    + Added Attack Component
    + Added RayAttack Component (inherits from Attack)
    + Added DamageReceiver Component (used by Attack components)

---
+++ Game/Explosions!/Assets/Code/Components/Attacks/Attack.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Code.Components.Attacks
+{
+    public abstract class Attack : MonoBehaviour
+    {
+        [SerializeField]
+        private LayerMask mask;
+        //TODO: Allow specifying a "target", which could be a rigidbody or simply a location
+
+        public abstract void Use(int damage, int range);
+
+        protected LayerMask Mask { get { return mask; } }
+    }
+}
---
+++ Game/Explosions!/Assets/Code/Components/Attacks/RayAttack.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Components.Attacks
+{
+    public class RayAttack : Attack
+    {
+        public override void Use(int damage, int range)
+        {
+            RaycastHit hit;
+            Vector3 dir = transform.forward; //Default to straight forward
+            //TODO: Ray cast to target position
+
+            //Raycast 
+            if (Physics.Raycast(transform.position, dir, out hit, Mask) && 
+                InRange(hit.transform.position, range)) //Range check
+            {
+                ApplyDamage(hit.collider.gameObject, damage);
+            }
+        }
+
+        /// <summary>
+        /// Checks if squared distance is less than squared range
+        /// Magnitude is not used because square root is much less efficient than multipling the range
+        /// </summary>
+        /// <param name="hitPos">The position of the hit</param>
+        /// <param name="range">Maximum range</param>
+        /// <returns>Returns whether or not the hit position is within the maximum range</returns>
+        private bool InRange(Vector3 hitPos, int range)
+        {
+            return (hitPos - transform.position).sqrMagnitude < (range * range); 
+        }
+
+        /// <summary>
+        /// Applies damage to the targets DamageReceiver
+        /// Returns if
+        /// </summary>
+        /// <param name="go">GameObject instance (Could be null)</param>
+        /// <param name="damage"></param>
+        /// <returns>How much damage is actually dealt</returns>
+        private int ApplyDamage(GameObject go, int damage)
+        {
+            int ret = 0;
+            DamageReceiver recv;
+            if (go != null)
+            {
+                recv = go.GetComponent<DamageReceiver>();
+                if (recv != null)
+                {
+                    ret = recv.ReceiveDamage(damage);
+                }
+            }
+            return ret;
+        }
+    }
+}
