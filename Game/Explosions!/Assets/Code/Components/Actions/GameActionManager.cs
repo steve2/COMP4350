9d20daa9c6b38c8a65d8602856c0dac22259fc94
Eric Welwood <deusecww@hotmail.com>
2014-03-07 13:10:28 -0600

    Implemented GameActionManager
    + Added GameActionManager that manages all actions that an entity can
    perform
    + Added GameActionController that maps controls to actions
    + Added GameActionButtons that renders buttons the player can press to
    perform actions

---
+++ Game/Explosions!/Assets/Code/Components/Actions/GameActionManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+namespace Assets.Code.Components.Actions
+{
+    public class GameActionManager : MonoBehaviour, IEnumerable<string>
+    {
+        private Dictionary<string, GameAction> actions;
+
+        public void Start()
+        {
+            actions = new Dictionary<string, GameAction>();
+            var myActions = GetComponents<GameAction>(); //Get our own actions
+            foreach (GameAction action in myActions)
+            {
+                AddAction(action);
+            }
+        }
+
+        //TODO: How will we group actions?
+        public void Perform(string name)
+        {
+            GameAction action;
+            if (actions.TryGetValue(name, out action))
+            {
+                action.Perform();
+            }
+        }
+
+        //TODO: What if we get a name conflict?
+        public bool AddAction(GameAction action)
+        {
+            //TODO: Just ignore duplicate names?
+            if(actions.ContainsKey(action.name))
+            {
+                return false;
+            }
+
+            action.Source = this.gameObject;
+            actions.Add(action.Name, action);
+            return true;
+        }
+
+        public bool RemoveAction(GameAction action)
+        {
+            action.Source = null;
+            return actions.Remove(action.Name);
+        }
+
+        //TODO: Return success?
+        public void AddActions(IEnumerable<GameAction> actions)
+        {
+            foreach (GameAction action in actions)
+            {
+                AddAction(action);
+            }
+        }
+
+        //TODO: Return success?
+        public void RemoveActions(IEnumerable<GameAction> actions)
+        {
+            foreach (GameAction action in actions)
+            {
+                RemoveAction(action);
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return actions.Keys.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Game/Explosions!/Assets/Code/View/GameActionButtons.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Plugins;
+
+[RequireComponent(typeof(GameActionController))]
+public class GameActionButtons : MonoBehaviour 
+{
+    private GameActionController controls;
+
+    public void Start()
+    {
+        controls = GetComponent<GameActionController>();
+    }
+
+    // Called when the GUI is rendered
+    void OnGUI()
+    {
+        GUILayout.BeginArea(GUIPlus.LayoutRect(0.5f, 0.05f, GUIAlign.Bottom));
+        GUILayout.BeginHorizontal("Actions");
+        foreach (string action in controls)
+        {
+            if (GUILayout.RepeatButton(action, GUILayout.Width(80), GUILayout.Height(40)))
+            {
+                controls.Perform(action);
+            }
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+    }
+}
+
