af3d37b8e75c49ce6ebc202be7f60209c7c2635a
Eric Welwood <deusecww@hotmail.com>
2014-03-07 13:03:21 -0600

    Started GUIPlus
    + Added GUIPlus plugin to add support for relatively sized and aligned
    layouts
    + Implemented Align = Bottom
    *None of the other alignments are implemented yet!

---
+++ Game/Explosions!/Assets/Plugins/GUIPlus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Plugins
+{
+    public enum GUIAlign
+    {
+        Top,
+        TopLeft,
+        Left,
+        BottomLeft,
+        Bottom,
+        BottomRight,
+        Right,
+        Center
+    }
+
+    public static class GUIPlus
+    {
+
+        public static Rect LayoutRect(float percentWidth, float percentHeight, GUIAlign align)
+        {
+            float width = Screen.width * percentWidth;
+            float height = Screen.height * percentHeight;
+            return new Rect(GetLeft(width, align), GetTop(height, align), width, height);
+        }
+
+        private static float GetLeft(float width, GUIAlign align)
+        {
+            switch (align)
+            {
+                case GUIAlign.Bottom:
+                    return (Screen.width - width) / 2;
+                default:
+                    return 0;
+            }
+        }
+
+        private static float GetTop(float height, GUIAlign align)
+        {
+            switch (align)
+            {
+                case GUIAlign.Bottom:
+                    return Screen.height - height;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
