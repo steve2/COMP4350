d7c45ccd36cd3968b20f2f15d295a0303759280d
Eric Welwood <deusecww@hotmail.com>
2014-03-09 15:49:58 -0500

    Fixed RayAttack Range
    = Fixed Physics.Raycast call
    - Removed InRange (Not needed)
    - Commented out InRange tests

--- Game/Explosions!/Assets/Code/Components/Actions/RayAttack.cs
+++ Game/Explosions!/Assets/Code/Components/Actions/RayAttack.cs
@@ -33,7 +33,6 @@ namespace Assets.Code.Components.Actions
         //TODO: Exclude this if we're not in debug mode
         public void Update()
         {
-            Debug.DrawRay(lastRay.origin, lastRay.direction, Color.red);
         }
 
         public Vector3 StartPosition
@@ -57,30 +56,16 @@ namespace Assets.Code.Components.Actions
 
             //TODO: Ray cast to target position (Change direction
             //Raycast 
-            Ray ray = new Ray(StartPosition, dir * Range);
+            Ray ray = new Ray(StartPosition, dir);
+            Debug.DrawRay(ray.origin, ray.direction*Range, Color.red, 1); //Draw a ray for 1 second
             this.lastRay = ray; //TODO: Maybe build a queue for the view?
-            if (Physics.Raycast(ray, out hit, mask) && 
-                InRange(hit.transform.position, Range)) //Range check
+            if (Physics.Raycast(ray, out hit, Range, mask))
             {
                 ApplyDamage(hit.collider.gameObject, damage);
                 return true;
             }
 
             return false;
-        }
-
-        /// <summary>
-        /// Checks if squared distance is less than squared range
-        /// Magnitude is not used because square root is much less efficient than multipling the range
-        /// </summary>
-        /// <param name="pos">The final position that will be range checked</param>
-        /// <param name="range">Maximum range</param>
-        /// <returns>Returns whether or not the hit position is within the maximum range</returns>
-        //TODO: Make this a "Condition"?
-        public bool InRange(Vector3 pos, int range)
-        {
-			Vector3 dist = pos - transform.position;
-            return (dist).sqrMagnitude <= (range * range); 
         }
 
         /// <summary>
--- Game/Explosions!/Assets/Code/Editor/Tests/Components/Actions/TestRayAttack.cs
+++ Game/Explosions!/Assets/Code/Editor/Tests/Components/Actions/TestRayAttack.cs
@@ -100,94 +100,95 @@ namespace Assets.Code.Editor.Tests.Components.Actions
         } 
         #endregion
 
+        //TODO: Remove
         #region InRange
-        private void TestRelativeRangeTrue(GameObject attacker, RayAttack attack, int range, int distance)
-        {
-            Vector3 attackerPos = attacker.transform.position;
-            Assert.IsTrue(attack.InRange(attackerPos + attacker.transform.forward * distance, range));
-            Assert.IsTrue(attack.InRange(attackerPos - (attacker.transform.forward * distance), range)); //back
-            Assert.IsTrue(attack.InRange(attackerPos + attacker.transform.right * distance, range));
-            Assert.IsTrue(attack.InRange(attackerPos - (attacker.transform.right * distance), range)); //left
-        }
+        //private void TestRelativeRangeTrue(GameObject attacker, RayAttack attack, int range, int distance)
+        //{
+        //    Vector3 attackerPos = attacker.transform.position;
+        //    Assert.IsTrue(attack.InRange(attackerPos + attacker.transform.forward * distance, range));
+        //    Assert.IsTrue(attack.InRange(attackerPos - (attacker.transform.forward * distance), range)); //back
+        //    Assert.IsTrue(attack.InRange(attackerPos + attacker.transform.right * distance, range));
+        //    Assert.IsTrue(attack.InRange(attackerPos - (attacker.transform.right * distance), range)); //left
+        //}
 
-        private void TestRelativeRangeFalse(GameObject attacker, RayAttack attack, int range, int distance)
-        {
-            Vector3 attackerPos = attacker.transform.position;
-            Assert.IsFalse(attack.InRange(attackerPos + attacker.transform.forward * distance, range));
-            Assert.IsFalse(attack.InRange(attackerPos - (attacker.transform.forward * distance), range)); //back
-            Assert.IsFalse(attack.InRange(attackerPos + attacker.transform.right * distance, range));
-            Assert.IsFalse(attack.InRange(attackerPos - (attacker.transform.right * distance), range)); //left
-        }
+        //private void TestRelativeRangeFalse(GameObject attacker, RayAttack attack, int range, int distance)
+        //{
+        //    Vector3 attackerPos = attacker.transform.position;
+        //    Assert.IsFalse(attack.InRange(attackerPos + attacker.transform.forward * distance, range));
+        //    Assert.IsFalse(attack.InRange(attackerPos - (attacker.transform.forward * distance), range)); //back
+        //    Assert.IsFalse(attack.InRange(attackerPos + attacker.transform.right * distance, range));
+        //    Assert.IsFalse(attack.InRange(attackerPos - (attacker.transform.right * distance), range)); //left
+        //}
 
-        [Test]
-        public void TestInsideRange()
-        {
-            GameObject attacker = Create();
-            RayAttack attack = attacker.GetComponent<RayAttack>();
-            TestRelativeRangeTrue(attacker, attack, 500, 400);
-        }
+        //[Test]
+        //public void TestInsideRange()
+        //{
+        //    GameObject attacker = Create();
+        //    RayAttack attack = attacker.GetComponent<RayAttack>();
+        //    TestRelativeRangeTrue(attacker, attack, 500, 400);
+        //}
 
-        [Test]
-        public void TestInsideRangeMoved()
-        {
-            GameObject attacker = Create();
-            RayAttack attack = attacker.GetComponent<RayAttack>();
-            attacker.transform.position = new Vector3(10, 7, 30); //Position shouldn't matter
-            TestRelativeRangeTrue(attacker, attack, 500, 400);
-        }
+        //[Test]
+        //public void TestInsideRangeMoved()
+        //{
+        //    GameObject attacker = Create();
+        //    RayAttack attack = attacker.GetComponent<RayAttack>();
+        //    attacker.transform.position = new Vector3(10, 7, 30); //Position shouldn't matter
+        //    TestRelativeRangeTrue(attacker, attack, 500, 400);
+        //}
 
-        [Test]
-        public void TestExactRange()
-        {
-            GameObject attacker = Create();
-            RayAttack attack = attacker.GetComponent<RayAttack>();
-            TestRelativeRangeTrue(attacker, attack, 300, 300);
-        }
+        //[Test]
+        //public void TestExactRange()
+        //{
+        //    GameObject attacker = Create();
+        //    RayAttack attack = attacker.GetComponent<RayAttack>();
+        //    TestRelativeRangeTrue(attacker, attack, 300, 300);
+        //}
 
-        [Test]
-        public void TestExactRangeMoved()
-        {
-            GameObject attacker = Create();
-            RayAttack attack = attacker.GetComponent<RayAttack>();
-            attacker.transform.position = new Vector3(3, -10, 5); //Position shouldn't matter
-            TestRelativeRangeTrue(attacker, attack, 300, 300);
-        }
+        //[Test]
+        //public void TestExactRangeMoved()
+        //{
+        //    GameObject attacker = Create();
+        //    RayAttack attack = attacker.GetComponent<RayAttack>();
+        //    attacker.transform.position = new Vector3(3, -10, 5); //Position shouldn't matter
+        //    TestRelativeRangeTrue(attacker, attack, 300, 300);
+        //}
 
-        [Test]
-        public void TestOutsideRange()
-        {
-            GameObject attacker = Create();
-            RayAttack attack = attacker.GetComponent<RayAttack>();
-            TestRelativeRangeFalse(attacker, attack, 300, 500);
-        }
+        //[Test]
+        //public void TestOutsideRange()
+        //{
+        //    GameObject attacker = Create();
+        //    RayAttack attack = attacker.GetComponent<RayAttack>();
+        //    TestRelativeRangeFalse(attacker, attack, 300, 500);
+        //}
 
-        [Test]
-        public void TestOutsideRangeMoved()
-        {
-            GameObject attacker = Create();
-            RayAttack attack = attacker.GetComponent<RayAttack>();
-            attacker.transform.position = new Vector3(38, 5, -5); //Position shouldn't matter
-            TestRelativeRangeFalse(attacker, attack, 300, 500);
-        }
+        //[Test]
+        //public void TestOutsideRangeMoved()
+        //{
+        //    GameObject attacker = Create();
+        //    RayAttack attack = attacker.GetComponent<RayAttack>();
+        //    attacker.transform.position = new Vector3(38, 5, -5); //Position shouldn't matter
+        //    TestRelativeRangeFalse(attacker, attack, 300, 500);
+        //}
 
-        [Test]
-        public void TestCloseRanges()
-        {
-            GameObject attacker = Create();
-            RayAttack attack = attacker.GetComponent<RayAttack>();
-            TestRelativeRangeFalse(attacker, attack, 300, 301);
-            TestRelativeRangeTrue(attacker, attack, 300, 299);
-        }
+        //[Test]
+        //public void TestCloseRanges()
+        //{
+        //    GameObject attacker = Create();
+        //    RayAttack attack = attacker.GetComponent<RayAttack>();
+        //    TestRelativeRangeFalse(attacker, attack, 300, 301);
+        //    TestRelativeRangeTrue(attacker, attack, 300, 299);
+        //}
 
-        [Test]
-        public void TestCloseRangesMoved()
-        {
-            GameObject attacker = Create();
-            RayAttack attack = attacker.GetComponent<RayAttack>();
-            attacker.transform.position = new Vector3(-4, 50, 1); //Position shouldn't matter
-            TestRelativeRangeFalse(attacker, attack, 300, 301);
-            TestRelativeRangeTrue(attacker, attack, 300, 299);
-        } 
+        //[Test]
+        //public void TestCloseRangesMoved()
+        //{
+        //    GameObject attacker = Create();
+        //    RayAttack attack = attacker.GetComponent<RayAttack>();
+        //    attacker.transform.position = new Vector3(-4, 50, 1); //Position shouldn't matter
+        //    TestRelativeRangeFalse(attacker, attack, 300, 301);
+        //    TestRelativeRangeTrue(attacker, attack, 300, 299);
+        //} 
         #endregion
 
     }
