080e7df20507d9d74aad09b8aa6def9213955557
Eric Welwood <deusecww@hotmail.com>
2014-03-09 16:27:04 -0500

    Implemented EquipmentManager Prefab instantiation
    + Added InitPrefab
    + Added DestroyPrefab
    + (Unrelated) Added hack for item usage
    *TODO: Remove hack when Inventory/Equipment loading is implemented

--- Game/Explosions!/Assets/Code/Components/EquipmentManager.cs
+++ Game/Explosions!/Assets/Code/Components/EquipmentManager.cs
@@ -15,7 +15,7 @@ namespace Assets.Code.Components
         private GameActionManager actionMgr;
         private Equipment equipment;
         [SerializeField]
-        private Item[] defaultEquipment; //Loaded from the editor
+        private List<Item> defaultEquipment; //Loaded from the editor
         private Inventory inventory;
 
         /** Initialization **/
@@ -26,14 +26,7 @@ namespace Assets.Code.Components
             attributeMngr = GetComponent<AttributeManager>();
             actionMgr = GetComponent<GameActionManager>();
 
-            //Load in some default items from editor without contacting server (Ex: Demo)
-            if (defaultEquipment != null)
-            {
-                for (int i = 0; i < defaultEquipment.Length; i++)
-                {
-                    Equip(defaultEquipment[i], (Slot)i);
-                }
-            }
+			Invoke ("DefaultEquipmentHACK", 0.5f);
         }
 
         /**
@@ -55,6 +48,7 @@ namespace Assets.Code.Components
 				equipment.SetSlot(whatSlot, null);
                 attributeMngr.SubtractAttributes(equipped);
                 actionMgr.RemoveActions(equipped.Actions);
+                DestroyPrefab(equipped);
                 return true;
             }
             return false;
@@ -76,21 +70,48 @@ namespace Assets.Code.Components
          */
         public bool Equip(Item toEquip, Slot whatSlot)
         {
-            if (toEquip == null || !inventory.Contains(toEquip))
+            if (toEquip == null || !inventory.Contains(toEquip)) //TODO: THIS WILL BREAK
             {
                 return false;
             }
-			if (toEquip.Type.IsSlotAllowed (whatSlot))
+			if (toEquip.Type.IsSlotAllowed (whatSlot)) //TODO: THIS WILL BREAK
 			{
             	Dequip(whatSlot);
-				equipment.SetSlot(whatSlot, toEquip);
-				inventory.Remove (toEquip);
-				attributeMngr.AddAttributes(toEquip);
-                actionMgr.AddActions(toEquip.Actions);
+                Item actualItem = InitPrefab(toEquip);
+				equipment.SetSlot(whatSlot, actualItem);
+				inventory.Remove (actualItem);
+				attributeMngr.AddAttributes(actualItem);
+                actionMgr.AddActions(actualItem.Actions);
 				return true;
 			}
 			return false;
         }
 
-    }
+        protected virtual Item InitPrefab(Item prefab)
+        {
+            Item instance = (Item)Instantiate(prefab, transform.position, transform.rotation);
+            instance.transform.parent = this.transform;
+            return instance;
+        }
+
+        protected virtual void DestroyPrefab(Item instance)
+        {
+            Destroy(instance);
+        }
+
+		private void DefaultEquipmentHACK()
+		{
+			//HACK: REMOVE THIS when loading is complete (Use the injected server instead)
+			//Load in some default items from editor without contacting server (Ex: Demo)
+			if (defaultEquipment != null)
+			{
+				foreach(Item item in defaultEquipment)
+				{
+                    Item actualItem = InitPrefab(item);
+                    attributeMngr.AddAttributes(actualItem);
+					actionMgr.AddActions(actualItem.Actions);
+				}
+			}
+		}
+	}
 }
