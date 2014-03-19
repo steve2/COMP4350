37a5de2adc3ce4ca26f33494c0e6e86962d4ce9e
Eric Welwood <deusecww@hotmail.com>
2014-03-18 20:04:37 -0500

    Created Mission Manager
    + Added ability to Show/Hide character
    *This could eventually manage objectives, etc

---
+++ Game/Explosions!/Assets/Code/Components/MissionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Code.Components
+{
+    public class MissionManager : GameComponent
+    {
+        public override void Start()
+        {
+			base.Start();
+            GameInst.ShowCharacter();
+        }
+
+        public void Destroy()
+        {
+            GameInst.HideCharacter();
+        }
+    }
+}
